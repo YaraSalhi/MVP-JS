17ab9eb0b249473892eac6487a2b521e57d50655
NourAn (N.A) <noranadel@gmail.com>
2019-12-06 14:35:44 +0200

    itempage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import Slider from './slider/slider'
+import Itempage from './itempage';
+
 import * as serviceWorker from './serviceWorker';
 
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Itempage />, document.getElementById('Itempage'));
+//ReactDOM.render(<Slider />, document.getElementById('Slider'));
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
