41fad7cd834efa62230ae04514f9f8312f8a6120
AAlbarqi <ashjanalbarqi@gmail.com>
2019-12-07 11:56:25 +0200

    Ash

--- src/Item.js
+++ src/Item.js
@@ -1,6 +1,4 @@
 import React, { Component, useState, useEffect } from "react";
-import $ from "jquery";
-import { createStore } from "redux";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
