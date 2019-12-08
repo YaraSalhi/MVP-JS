f20953820ac2703f297ed2ace1e5c8ebba55a488
YaraSalhi <yara.salhi@outlook.sa>
2019-12-08 00:27:45 -0800

    updated schema

--- models/item.js
+++ models/item.js
@@ -6,287 +6,148 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
   console.log(" database connection succeded");
 });
-const reservationsSchema = new Schema ({
-    hallsImg:String,
-    saloonsImg:String,
-    flowersImg:String,
-    tuxedosImg:String,
-    dressesImg:String,
-    cakeImg:String,
-    restaurantsImg:String,
-    extraImg:String
+const userSchema = new Schema ({
+    name:String,
+    email:String,
+    password:String,
+    role:String,
+
 })
 
-const userinfo = new Schema({
+const ownerSchema = new Schema({
   name: String,
+  shopName: String,
   email: String,
   password: String,
   location: String,
-  creditCard: Number
+  category:String
 });
 
-const restaurants = new Schema({
+const customerSchema = new Schema({
   name: String,
-  location: String,
-  contactInfo: Number,
-  price: Number,
-  owner: String
+  email: String,
+  password: String,
+  mobile: Number
 });
-const cakeShop = new Schema({
+const ShopsSchema = new Schema({
   name: String,
-  location: String,
-  contactInfo: Number,
-  price: Number,
-  owner: String
+  description:String,
+
 });
-const saloons = new Schema({
+const itemSchema = new Schema({
   name: String,
   location: String,
   contactInfo: Number,
   price: Number,
-  owner: String
-});
-const flowersShops = new Schema({
-  name: String,
-  location: String,
-  contactInfo: Number,
-  price: Number,
-  owner: String
-});
-const dressesShops = new Schema({
-  name: String,
-  location: String,
-  contactInfo: Number,
-  price: Number,
-  owner: String
-});
-const menShops = new Schema({
-  name: String,
-  location: String,
-  contactInfo: Number,
-  price: Number,
-  owner: String
-});
-const halls = new Schema({
-  name: String,
-  location: String,
-  contactInfo: Number,
-  price: Number,
-  owner: String
-});
-const extra = new Schema({
-  name: String,
-  location: String,
-  contactInfo: Number,
-  price: Number,
-  owner: String
+  owner: String,
+  description:String,
+
 });
 
-const reservationsModel = mongoose.model("Items", reservationsSchema);
-const UserinfoModel = mongoose.model("userinfo", userinfo);
-const restaurantsModel = mongoose.model("restaurants", restaurants);
-const cakeShopModel = mongoose.model("cakeShop", cakeShop);
-const saloonsModel = mongoose.model("saloons", saloons);
-const flowersShopsModel = mongoose.model("flowersShops", flowersShops);
-const dressesShopsModel = mongoose.model("dressesShops", dressesShops);
-const menShopsModel = mongoose.model("menShops", menShops);
-const hallsModel = mongoose.model("halls", halls);
-const extraModel = mongoose.model("extra", extra);
+
+const UserModel = mongoose.model("user", userSchema);
+const OwnerModel = mongoose.model("owner", ownerSchema);
+const CustomerModel = mongoose.model("customer", customerSchema);
+const ShopsModel = mongoose.model("shops", ShopsSchema);
+const ItemModel = mongoose.model("item", itemSchema);
 
 
 
 
-reservation1 = new reservationsModel({
-    hallsImg:"String",
-    saloonsImg:"String",
-    flowersImg:"String",
-    tuxedosImg:"String",
-    dressesImg:"String",
-    cakeImg:"String",
-    restaurantsImg:"String",
-    extraImg:"String"  
+user1 = new UserModel({
+  name:"String",
+  email:"String",
+  password:"String",
+  role:"String",
 });
-userinfo1 = new  UserinfoModel({
-    name : "String",
-    location:"String",
-    contactInfo:1,
-    price : 2,
-    owner:"String"
+owner1 = new  OwnerModel({
+  name: "String",
+  shopName: "String",
+  email: "String",
+  password: "String",
+  location: "String",
+  category:"String"
 })
-restaurants1 = new  restaurantsModel({
-    name : "String",
-    location:"String",
-    contactInfo:1,
-    price : 2,
-    owner:"String"
+customer1 = new  CustomerModel({
+  name: "String",
+  email: "String",
+  password: "String",
+  mobile: 3
 })
-cakeShop1 = new  cakeShopModel({
-    name : "String",
-    location:"String",
-    contactInfo:1,
-    price : 2,
-    owner:"String"
+shops1 = new ShopsModel({
+  name: "String",
+  description:"String",
 })
-saloons1 = new  saloonsModel({
-    name : "String",
-    location:"String",
-    contactInfo:1,
-    price : 2,
-    owner:"String"
+item1 = new  ItemModel({
+  name: "String",
+  location: "String",
+  contactInfo: 4,
+  price: 5,
+  owner: "String",
+  description:"String",
 })
-flowersShops1 = new  flowersShopsModel({
-    name : "String",
-    location:"String",
-    contactInfo:1,
-    price : 2,
-    owner:"String"
-})
-dressesShops1 = new  dressesShopsModel({
-    name : "String",
-    location:"String",
-    contactInfo:1,
-    price : 2,
-    owner:"String"
-})
-menShops1 = new  menShopsModel({
-    name : "String",
-    location:"String",
-    contactInfo:1,
-    price : 2,
-    owner:"String"
-})
-hall1 = new  hallsModel({
-    name : "String",
-    location:"String",
-    contactInfo:1,
-    price : 2,
-    owner:"String"
-})
-extra1 = new  extraModel({
-    name : "String",
-    location:"String",
-    contactInfo:1,
-    price : 2,
-    owner:"String"
-})
+
 
     
-// reservation1.save((error,result)=>{
+// user1.save((error,result)=>{
 //       if(error){
 //         console.log("errrrrror",error
 //         )
 //       }
 //       else{
-//       console.log("reservation done")
+//       console.log("user done")
 //       }
 //       });
 
 
-// userinfo1.save((error,result)=>{
+//    owner1.save((error,result)=>{
+//       if(error){
+//        console.log("restaurants error",error
+//       )
+//       }
+//       else{
+//       console.log("owner done")
+//       }
+//       });
+
+// customer1.save((error,result)=>{
 //         if(error){
 //         console.log("user error",error
 //         )
 //         }
 //         else{
-//         console.log("user done")
+//         console.log("customer done")
 //         }
 //         });
 
 
-// restaurants1.save((error,result)=>{
-//     if(error){
-//      console.log("restaurants error",error
-//     )
-//     }
-//     else{
-//     console.log("restaurants done")
-//     }
-//     });
-
-
-// cakeShop1.save((error,result)=>{
+// shops1.save((error,result)=>{
 //      if(error){
 //     console.log("cakeshops error",error
 //         )
 //         }
 //     else{
-//     console.log("cakeshops done")
+//     console.log("shops done")
 //     }
 //     });
 
 
-// saloons1.save((error,result)=>{
+// item1.save((error,result)=>{
 //     if(error){
 //      console.log("saloons error",error
 //         )
 //         }
 //     else{
-//     console.log("saloons done")
-//     }
-//     });
-                    
-// flowersShops1.save((error,result)=>{
-//     if(error){
-//     console.log("flowers error",error
-//     )
-//     }
-//     else{
-//     console.log("flowers done")
+//     console.log("item done")
 //     }
 //     });
 
-// dressesShops1.save((error,result)=>{
-//     if(error){
-//      console.log("dresses error",error
-//         )
-//         }
-//         else{
-//         console.log("dresses done")
-//         }
-//         });
 
-// menShops1.save((error,result)=>{
-//     if(error){
-//     console.log("menshops error",error
-//     )
-//     }
-//     else{
-//     console.log("menshops done")
-//     }
-//     });
-// hall1.save((error,result)=>{
-//     if(error){
-//     console.log("hall error",error
-//      )
-//     }
-//     else{
-//     console.log("hall done")
-//     }
-//     });
+module.exports.UserModel = UserModel;
+module.exports.OwnerModel=OwnerModel;
+module.exports.CustomerModel=CustomerModel;
+module.exports.ShopsModel=ShopsModel;
+module.exports.ItemModel=ItemModel;
 
-//  extra1.save((error,result)=>{
-//     if(error){
-//     console.log("extra error",error
-//      )
-//     }
-
-    
-//     else{
-//     console.log("extra done")
-//     }
-//     });
-      
-
-
-module.exports.reservationsModel = reservationsModel;
-module.exports.UserinfoModel=UserinfoModel;
-module.exports.restaurantsModel=restaurantsModel;
-module.exports.cakeShopModel=cakeShopModel;
-module.exports.saloonsModel=saloonsModel;
-module.exports.flowersShopsModel=flowersShopsModel;
-module.exports.dressesShopsModel=dressesShopsModel;
-module.exports.menShopsModel=menShopsModel;
-module.exports.hallsModel=hallsModel;
-module.exports.extraModel=extraModel;
 
 
